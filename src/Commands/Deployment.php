6ef25699fcd3088b5eb4453dbf189166bb6d7d5f
Patrick Kumm <patrick.kumm@brandung.de>
2019-04-11 17:11:19 +0200

    install, activate and update plugins automatically

---
+++ src/Commands/Deployment.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace ShopwareLabs\Plugin\SwagDeployment\Commands;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Plugin;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+/**
+ * Class Deployment
+ *
+ * @package ShopwareLabs\Plugin\SwagDeployment\Commands
+ */
+class Deployment extends Command
+{
+    /**
+     * @var EntityRepositoryInterface
+     */
+    protected $pluginRepository;
+
+    /**
+     * Deployment constructor.
+     *
+     * @param string                    $name
+     * @param EntityRepositoryInterface $pluginRepository
+     */
+    public function __construct(string $name, EntityRepositoryInterface $pluginRepository)
+    {
+        parent::__construct($name);
+        $this->pluginRepository = $pluginRepository;
+    }
+
+    /**
+     *
+     */
+    protected function configure(): void
+    {
+        $this->setName('swag:deploy');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|void|null
+     * @throws \Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Refreshing plugins...');
+        $command = $this->getApplication()->find('plugin:refresh');
+        $command->run(new ArrayInput([]), $output);
+
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('managedByComposer', 1));
+        $plugins = $this->pluginRepository->search($criteria, Context::createDefaultContext());
+
+        /** @var Plugin $plugin */
+        foreach ($plugins as $plugin) {
+            $pluginName = $plugin->getName();
+            $helper     = $this->getHelper('question');
+
+            $question   = new ConfirmationQuestion('Install and activate ' . $pluginName . ' ?(Y/n) ', true, '/^(y|j)/i');
+            if ($helper->ask($input, $output, $question)) {
+                $arguments = new ArrayInput(['plugins' => [$pluginName], '--activate' => true]);
+                $command   = $this->getApplication()->find('plugin:install');
+                $command->run($arguments, $output);
+            }
+
+            $question = new ConfirmationQuestion('Update ' . $pluginName . '? (Y/n) ', true, '/^(y|j)/i');
+            if ($helper->ask($input, $output, $question)) {
+                $arguments = new ArrayInput(['plugins' => [$pluginName]]);
+                $command   = $this->getApplication()->find('plugin:update');
+                $command->run($arguments, $output);
+            }
+        }
+    }
+}
---
+++ src/SwagDeployment.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace ShopwareLabs\Plugin\SwagDeployment;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+
+class SwagDeployment extends Plugin
+{
+
+}
